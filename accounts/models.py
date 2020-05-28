e87973ee4fdbd99fd718f7a91f072918ade0ef38
iopasd753951 <iopasd753951@gmail.com>
2020-05-28 15:58:24 +0900

    [추가] accounts models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
 # Create your models here.
+class Accounts(models.Model):
+    email = models.EmailField(unique=True)
+    nick_name = models.CharField(validators=[MinLengthValidator(2)], max_length=8)
+    password = models.CharField(validators=[MinLengthValidator(5)], max_length=100)
+    phone_number = models.CharField(max_length=12, blank=False, unique=True)

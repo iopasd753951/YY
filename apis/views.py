8710ab56a424fe16347ad8e9a80d24cd6e176691
iopasd753951 <iopasd753951@gmail.com>
2020-06-11 08:52:18 +0900

    [추가] apis 디렉토리 --> base user api

---
+++ apis/views.py
@@ -0,0 +1,18 @@
+from django.http import JsonResponse
+from django.views import View
+
+
+class BaseView(View):
+    @staticmethod
+    def response(data={}, message="", status=200):
+
+        result = {
+            'data': data,
+            'message': message,
+        }
+
+        return JsonResponse(result, status)
+
+
+class UserCreateView(BaseView):
+    @method-decorator(csrf_exempt)

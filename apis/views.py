79d60e480eca8a7cd8eb0ce6fb831e7b6d8dfb72
iopasd753951 <iopasd753951@gmail.com>
2020-06-11 10:09:28 +0900

    [추가] Ajax CSRF 예외처리

--- apis/views.py
+++ apis/views.py
@@ -1,5 +1,9 @@
 from django.http import JsonResponse
 from django.views import View
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+from django.contrib.auth.models import User
+from django.db import IntegrityError
 
 
 class BaseView(View):
@@ -15,4 +19,16 @@ class BaseView(View):
 
 
 class UserCreateView(BaseView):
-    @method-decorator(csrf_exempt)
+    @method_decorator(csrf_exempt) # ajax 통신시(POST요청) CSRF 토큰요구로 에러가 발생하는데 그것을 우회함
+    def dispatch(self, request, *args, **kwargs):
+        return super(UserCreateView, self).dispatch(request, *args, **kwargs)
+
+    def post(self, request):
+        user_name = request.POST.get('user_name', '')
+        password = request.POST.get('password', '')
+        email = request.POST.get('email', '')
+        phone_number = request.POST.get('phone_number')
+
+        user = User.objects.create_user(user_name, password, email, phone_number)
+
+        return self.response({'user.id': user.id})

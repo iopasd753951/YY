c1ef6c73b8309640e5d2325a756aab3ad470acf5
iopasd753951 <iopasd753951@gmail.com>
2020-06-11 10:13:34 +0900

    [추가] 프로젝트 include url설정

--- Yea_Yac/urls.py
+++ Yea_Yac/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
+    path('api-auth/', include('rest_framework.urls')),
     path('api/', include('accounts.api.urls')),
+    path('apis/', include('apis.urls')),
 ]

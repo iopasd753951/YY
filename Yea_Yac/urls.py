3bd66c02de14222dc40abc72ad525741df62fb53
iopasd753951 <iopasd753951@gmail.com>
2020-06-11 11:55:06 +0900

    [추가] contents app의 대한 urls(endpoint)

--- Yea_Yac/urls.py
+++ Yea_Yac/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from contents import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('api/', include('accounts.api.urls')),
     path('apis/', include('apis.urls')),
+    path('', views.HomeView.as_view(), name='contents_home'),
 ]

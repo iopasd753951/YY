b2e66bd1f38e8cedfc08016cec5f9b9a4617926f
iopasd753951 <iopasd753951@gmail.com>
2020-06-11 19:24:32 +0900

    [추가] login dispatch설정

--- contents/views.py
+++ contents/views.py
@@ -1,6 +1,14 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from django.utils.decorators import method_decorator
 
 
 class HomeView(TemplateView):
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super(HomeView, self).dispatch(request, *args, **kwargs)
+
     template_name = 'home.html'
+
+# dispatch : 요청이 get, post 같은 정상 메소드인지 확인 후 핸들러 함수로 request, *args, **kwargs를 넘겨줌

504ed53717679758b1330c2579b02b681435cc4d
iopasd753951 <iopasd753951@gmail.com>
2020-06-11 19:23:14 +0900

    [추가] login url을 TemplateView를 통해 Endpoint 설정

--- Yea_Yac/urls.py
+++ Yea_Yac/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 from contents import views
 
@@ -8,4 +9,5 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('apis/', include('apis.urls')),
     path('', views.HomeView.as_view(), name='contents_home'),
+    path('login/', TemplateView.as_view(template_name='login.html', name='login'))
 ]

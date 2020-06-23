45006f66bcb2b0214027d6008eb4f339ff545e8a
iopasd753951 <iopasd753951@gmail.com>
2020-06-23 13:41:11 +0900

    [수정] app name apis --> accounts

--- apis/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-
-from apis import views
-
-urlpatterns = [
-    path('v1/users/signup', views.UserSignupView.as_view(), name='apis_v1_user_signup'),
-    path('v1/users/login', views.UserLoginView.as_view(), name='apis_v1_user_login'),
-    path('v1/users/logout', views.UserLogoutView.as_view(), name='apis_v1_user_logout'),
-]

bfd5589cc35e265cd88d94b09486be7a178b0b5c
iopasd753951 <iopasd753951@gmail.com>
2020-06-23 13:43:26 +0900

    [추가] Acccounts 모델추가

---
+++ accounts/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.core.validators import MinLengthValidator
+
+
+class Accounts(models.Model):
+    email = models.EmailField()
+    name = models.CharField(validators=[MinLengthValidator(5)], max_length=255)
+    password = models.CharField(validators=[MinLengthValidator(5)], max_length=255)
+    phone = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)

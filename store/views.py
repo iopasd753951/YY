d0b0719b66f08194ced77425a810cca15d10ddd0
iopasd753951 <iopasd753951@gmail.com>
2020-08-03 01:19:31 +0900

    [추가] store search view 추가

--- store/views.py
+++ store/views.py
@@ -1,3 +1,20 @@
+from django.db.models import Q
+from django.http import JsonResponse
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+from yea_yac import exception
+from . import models
+
+
+class SearchView(View):
+    def get(self, request):
+        query = request.GET.get['query']
+
+        if models.Store.objects.filter(Q(store_name__contains=query) | Q(store_content__contains=query)):
+            filter_data = models.Store.objects.filter(Q(store_name__contains=query) | Q(store_content__contains=query))
+
+            return JsonResponse(list(filter_data.values()), status=200, safe=False)
+
+        else:
+            return exception.NotValueExists()

7e5d1f1bb786df185f4199fb69a977094ad9f0e7
iopasd753951 <iopasd753951@gmail.com>
2020-08-03 01:19:09 +0900

    [추가] store model 추가

--- store/models.py
+++ store/models.py
@@ -1,3 +1,13 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+from taggit.managers import TaggableManager
+from taggit.models import TagBase, TaggedItemBase
+
+
+class Store(models.Model):
+    store_id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    store_name = models.CharField(max_length=255)
+    store_content = models.TextField()
+    store_tag = TaggableManager()
+    created_at = models.DateTimeField(auto_now=True)
